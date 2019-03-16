da3f9e1fa26f5ed6224b38429ddfb39491c28f5a
Prodigy4532 <proddyy4532@yahoo.com>
2019-03-16 17:56:09 -0600

    Register things

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
 
     def new
         @token = Array.new(12){[*'0'..'9', *'a'..'z'].sample}.join
-        @url = "register.#{ORG::DOMAIN}"
+        @url = "register.play.#{ORG::DOMAIN}"
     end
 
     def create
